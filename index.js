c9146c64f290104b2f249776346ece55d1e6e50b
Artem Alieksieiev <artem.alieksieiev@gmail.com>
2024-03-10 21:38:57 +0200

    Added global project settings

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 /**
  * @format
  */
+import { AppRegistry } from 'react-native'
+import App from '@app/app'
+import { name as appName } from './app.json'
 
-import {AppRegistry} from 'react-native';
-import App from './App';
-import {name as appName} from './app.json';
+// eslint-disable-next-line react/prop-types
+function HeadlessCheck({ isHeadless }) {
+    if (isHeadless) {
+        return null
+    }
 
-AppRegistry.registerComponent(appName, () => App);
+    return <App />
+}
+
+AppRegistry.registerComponent(appName, () => HeadlessCheck)
